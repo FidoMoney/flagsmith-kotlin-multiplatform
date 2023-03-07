4d8980cd564d2b2b7587f64ce83cc3039cbfe816
idan <idan@fidocredit.com>
2023-03-07 16:23:00 +0200

    1.0.3.1
    - missing @Serializable

--- shared/src/commonMain/kotlin/com/flagsmith/entities/IdentityWithTraits.kt
+++ shared/src/commonMain/kotlin/com/flagsmith/entities/IdentityWithTraits.kt
@@ -1,5 +1,8 @@
 package com.flagsmith.entities
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class IdentityWithTraits(
     val identifier: String,
     val traits: List<Trait>
